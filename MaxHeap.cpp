56226b6860863500afca50f176cd3d5396c36cf4
Jim0811 <168328299+Jim0811@users.noreply.github.com>
2024-06-14 20:38:09 +0800

    add running time

--- MaxHeap.cpp
+++ MaxHeap.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 //col 0    1    2    3   4     5            6
 //    date open high low close daily_return intraday_return
-class MaxHeap{
+class MaxHeap {
 private:
     double** arr;
     int size;
 public:
-    MaxHeap(int n){
+    MaxHeap(int n) {
         arr = new double* [n];
-        for(int i=0;i<n;i++){
-            arr[i]=new double[7];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new double[7];
         }
         size = n;
     }
 
-    void insert(int i, double* a_row){
+    void insert(int i, double* a_row) {
         copy(a_row, a_row + 5, arr[i]); //把a_row的前五列資料複製到arr[i]裡面
     }
 
-    double get(int r,int c){
+    double get(int r, int c) {
         return arr[r][c];
     }
 
-    void MAX_HEAPIFY(int col, int n,int i){ //col是哪列資料 n是heap的大小 i是子樹的根
+    void MAX_HEAPIFY(int col, int n, int i) { //col是哪列資料 n是heap的大小 i是子樹的根
         int largest = i;
-        int left = 2*i+1;
-        int right = 2*i+2;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
         if (left < n && arr[left][col] > arr[largest][col])
             largest = left;
 
@@ -44,7 +44,7 @@ public:
 
     void heapSort(int col) { //col是哪列資料 
         // Build max heap
-        for (int i = size/2; i >= 0; i--){
+        for (int i = size / 2; i >= 0; i--) {
             MAX_HEAPIFY(col, size, i);
         }
         // Heap sort
@@ -55,24 +55,24 @@ public:
         }
     }
 
-    void daily_returns(){ // 要先sort by date才可以用，計算daily return並放在column 5
-        for(int i=1;i<size;i++){
-            arr[i][5] = (arr[i][4] - arr[i-1][4]) / arr[i-1][4]; //小數點 還不是百分比
+    void daily_returns() { // 要先sort by date才可以用，計算daily return並放在column 5
+        for (int i = 1; i < size; i++) {
+            arr[i][5] = (arr[i][4] - arr[i - 1][4]) / arr[i - 1][4]; //小數點 還不是百分比
         }
     }
 
-    void intraday_return(){ // 要先sort by date才可以用，計算intraday return並放在column 6
-        for(int i=0;i<size;i++){
+    void intraday_return() { // 要先sort by date才可以用，計算intraday return並放在column 6
+        for (int i = 0; i < size; i++) {
             arr[i][6] = (arr[i][4] - arr[i][1]) / arr[i][1]; //小數點 還不是百分比
         }
-    } 
+    }
 
-    void show(){ //測試用
-        for(int i=0;i<10;i++){
-            for(int j=0;j<5;j++){
-                if(j==0) cout << int(arr[i][j]) << " ";
+    void show() { //測試用
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j == 0) cout << int(arr[i][j]) << " ";
                 else cout << arr[i][j] << " ";
-                
+
             }
             cout << endl;
         }
--- main.cpp
+++ main.cpp
@@ -3,71 +3,73 @@
 #include <string>
 #include <cstring>
 #include <fstream>
+#include <chrono>
 #include "MaxHeap.cpp"
 using namespace std;
 
-int main(){
+int main() {
     //讀取檔案
     string line;
     int size = 0;
-    
-    ifstream in("C:/Users/72161/OneDrive/文件/GitHub/Data-final/data1.csv");
-    if (!in){
+
+    ifstream in("C:\\Users\\72161\\source\\repos\\DS2\\x64\\Debug\\data1.csv");
+    if (!in) {
         cout << "開啟檔案失敗！" << endl;
         exit(1);
-    }   
-    while (getline(in, line)){
+    }
+    while (getline(in, line)) {
         size++; //暫存資料筆數(包含重複日期)
     }
     in.close();
-    in.open("C:/Users/72161/OneDrive/文件/GitHub/Data-final/data1.csv");
-    if (!in){
+    in.open("C:\\Users\\72161\\source\\repos\\DS2\\x64\\Debug\\data1.csv");
+    if (!in) {
         cout << "開啟檔案失敗！" << endl;
         exit(1);
-    } 
+    }
 
-    string unique_date[100000];
+    string unique_date[11881];
     int i = 0;
-    while (getline(in, line)){
+    while (getline(in, line)) {
         bool is_unique = true;
-        for(int j=0;j<i;j++){
-            if(line.substr(0, line.find(",")) == unique_date[j]){
+        for (int j = 0; j < i; j++) {
+            if (line.substr(0, line.find(",")) == unique_date[j]) {
                 is_unique = false;
                 break;
             }
         }
-        if(is_unique){
+        if (is_unique) {
             unique_date[i] = line.substr(0, line.find(","));
             i++;
         }
     }
-    
+
     in.close();
-    in.open("C:/VScode C++ project/data structure/final_project/data1.csv");
-    if (!in){
+    in.open("C:\\Users\\72161\\source\\repos\\DS2\\x64\\Debug\\data1.csv");
+    if (!in) {
         cout << "開啟檔案失敗！" << endl;
         exit(1);
-    } 
+    }
+    auto start = chrono::high_resolution_clock::now();
     size = i;//unique date數量(沒有重複日期)
     MaxHeap heap(size);//建立MaxHeap array
     double temp[7];
     i = 0;
     int cut;
-    while (getline(in, line)){
+    while (getline(in, line)) {
         cut = line.find(",");
         bool is_unique = true;
-        for(int j=0;j<i;j++){
-            if(line.substr(0, cut) == unique_date[j]){
+        for (int j = 0; j < i; j++) {
+            if (line.substr(0, cut) == unique_date[j]) {
                 is_unique = false;
                 break;
             }
         }
-        if(!is_unique){
+        if (!is_unique) {
             continue;
         }
         temp[0] = stod(line.substr(0, cut));
         line = line.substr(cut + 1);
-        
+
         cut = line.find(",");
         temp[1] = stod(line.substr(0, cut));
         line = line.substr(cut + 1);
@@ -81,29 +83,29 @@ int main(){
         line = line.substr(cut + 1);
 
         temp[4] = stod(line);
-        
+
         heap.insert(i, temp);
         i++;
     }
     cout << "Task(A):" << endl;
     //(1) Determine how many unique dates are in the dataset.
-    cout << "(1) There are "  << size << " unique dates in the dataset." << endl;
+    cout << "(1) There are " << size << " unique dates in the dataset." << endl;
 
     //(2) Find the 10 smallest prices and which dates contain these smallest prices.
     heap.heapSort(4); //sort by close price
     cout << "(2) The 10 smallest prices are:" << endl;
-    for(int i=0;i<10;i++){
+    for (int i = 0; i < 10; i++) {
         cout << heap.get(i, 4) << " on date " << int(heap.get(i, 0)) << endl;
     }
-    
+
     //(3) Find the 10 largest prices and which dates contain these largest prices.
     cout << "(3) The 10 largest prices are:" << endl;
-    for(int i=size-10;i<size;i++){
+    for (int i = size - 1; i >= size - 10; i--) {
         cout << heap.get(i, 4) << " on date " << int(heap.get(i, 0)) << endl;
     }
 
     //(4) Find the median price and its occurring date
-    cout << "(4) The median price is " << heap.get(size/2, 4) << " and its occurring date is " << int(heap.get(size/2, 0)) << endl;
+    cout << "(4) The median price is " << heap.get(size / 2, 4) << " and its occurring date is " << int(heap.get(size / 2, 0)) << endl;
 
     //(5) Compute the daily return for every day (except the first day). Then determine what the 
     // maximum and minimum returns (return could be a negative value) are and on which day(s) they occur.
@@ -113,48 +115,51 @@ int main(){
     double min_return = INT_MAX;
     int max_index;
     int min_index;
-    for(int i=1;i<size;i++){
-        if(heap.get(i, 5) > max_return){
+    for (int i = 1; i < size; i++) {
+        if (heap.get(i, 5) > max_return) {
             max_return = heap.get(i, 5);
             max_index = i;
         }
-        if(heap.get(i, 5) < min_return){
+        if (heap.get(i, 5) < min_return) {
             min_return = heap.get(i, 5);
             min_index = i;
         }
     }
-    cout << "(5) The maximum return is " << max_return*100 << "%" << " and it occurs on " << int(heap.get(max_index, 0)) << endl;
-    cout << "    The minimum return is " << min_return*100 << "%" << " and it occurs on " << int(heap.get(min_index, 0)) << endl;
-    
+    cout << "(5) The maximum return is " << max_return * 100 << "%" << " and it occurs on " << int(heap.get(max_index, 0)) << endl;
+    cout << "    The minimum return is " << min_return * 100 << "%" << " and it occurs on " << int(heap.get(min_index, 0)) << endl;
+
     //(6) Compute the intraday return for every day. Then determine what the maximum and
     // minimum returns (return could be a negative value) are and on which day(s) they occur.
     heap.intraday_return();
     max_return = 0;
     min_return = INT_MAX;
-     for(int i=0;i<size;i++){
-        if(heap.get(i, 6) > max_return){
+    for (int i = 0; i < size; i++) {
+        if (heap.get(i, 6) > max_return) {
             max_return = heap.get(i, 6);
             max_index = i;
         }
-        if(heap.get(i, 6) < min_return){
+        if (heap.get(i, 6) < min_return) {
             min_return = heap.get(i, 6);
             min_index = i;
         }
     }
-    cout << "(6) The maximum return is " << max_return*100 << "%" << " and it occurs on " << int(heap.get(max_index, 0)) << endl;
-    cout << "    The minimum return is " << min_return*100 << "%" << " and it occurs on " << int(heap.get(min_index, 0)) << endl;
+    cout << "(6) The maximum return is " << max_return * 100 << "%" << " and it occurs on " << int(heap.get(max_index, 0)) << endl;
+    cout << "    The minimum return is " << min_return * 100 << "%" << " and it occurs on " << int(heap.get(min_index, 0)) << endl;
 
     //(10) Find the maximum, minimum and median prices using all the 4 columns of prices 
     // (i.e., Open_price, High_price, Low_price and Close_price) and determine on which date they occur.
     cout << "(10)" << endl;
-    string priceTypes[4] = {"Open_price", "High_price", "Low_price", "Close_price"};
+    string priceTypes[4] = { "Open_price", "High_price", "Low_price", "Close_price" };
     for (int i = 1; i <= 4; i++) {
         heap.heapSort(i);
-        cout << priceTypes[i-1] << ":" << endl;
-        cout << "The maximum is " << heap.get(size-1, i) << " and it occurs on date " << int(heap.get(size-1, 0)) << endl;
+        cout << priceTypes[i - 1] << ":" << endl;
+        cout << "The maximum is " << heap.get(size - 1, i) << " and it occurs on date " << int(heap.get(size - 1, 0)) << endl;
         cout << "The minimum is " << heap.get(0, i) << " and it occurs on date " << int(heap.get(0, 0)) << endl;
-        cout << "The median is " << heap.get(size/2, i) << " and it occurs on date " << int(heap.get(size/2, 0)) << endl;
+        cout << "The median is " << heap.get(size / 2, i) << " and it occurs on date " << int(heap.get(size / 2, 0)) << endl;
     }
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+    cout << "運行時間: " << duration.count() << " 秒" << std::endl;
     in.close();
     return 0;
 }
