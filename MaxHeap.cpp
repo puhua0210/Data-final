d0fa329e0fbc1a30c3052ee7234095cc95ce5433
puhua0210 <puhua0210@gmail.com>
2024-06-19 11:28:20 +0800

    array heap

--- MaxHeap.cpp
+++ MaxHeap.cpp
@@ -4,75 +4,69 @@ using namespace std;
 
 //col 0    1    2    3   4     5            6
 //    date open high low close daily_return intraday_return
+
+struct stock
+{
+    string date;
+    double open=0;
+    double high=0;
+    double low=0;
+    double close=0;
+};
+
 class MaxHeap{
 private:
-    double** arr;
+    stock* arr;
     int size;
 public:
     MaxHeap(int n){
-        arr = new double* [n];
-        for(int i=0;i<n;i++){
-            arr[i]=new double[7];
-        }
+        arr = new stock[n];
         size = n;
     }
 
-    void insert(int i, double* a_row){
-        copy(a_row, a_row + 5, arr[i]); //把a_row的前五列資料複製到arr[i]裡面
+    void insert(int i, stock s){
+        arr[i] = s;
     }
 
-    double get(int r,int c){
-        return arr[r][c];
+    stock get(int i){
+        return arr[i];
     }
 
-    void MAX_HEAPIFY(int col, int n,int i){ //col是哪列資料 n是heap的大小 i是子樹的根
+    void MAX_HEAPIFY(int n,int i){ //by close_price n是heap的大小 i是子樹的根
         int largest = i;
         int left = 2*i+1;
         int right = 2*i+2;
-        if (left < n && arr[left][col] > arr[largest][col])
+        if (left < n && arr[left].close > arr[largest].close)
             largest = left;
 
-        if (right < n && arr[right][col] > arr[largest][col])
+        if (right < n && arr[right].close > arr[largest].close)
             largest = right;
 
         // Swap and continue heapifying if root is not largest
         if (largest != i) {
             swap(arr[i], arr[largest]);
-            MAX_HEAPIFY(col, n, largest);
+            MAX_HEAPIFY(n, largest);
         }
     }
 
-    void heapSort(int col) { //col是哪列資料 
+    void heapSort() { //col是哪列資料 
         // Build max heap
         for (int i = size/2; i >= 0; i--){
-            MAX_HEAPIFY(col, size, i);
+            MAX_HEAPIFY(size, i);
         }
         // Heap sort
         for (int i = size - 1; i >= 0; i--) {
             swap(arr[0], arr[i]);
             // Heapify root element to get highest element at root again
-            MAX_HEAPIFY(col, i, 0);
+            MAX_HEAPIFY(i, 0);
         }
     }
-
-    void daily_returns(){ // 要先sort by date才可以用，計算daily return並放在column 5
-        for(int i=1;i<size;i++){
-            arr[i][5] = (arr[i][4] - arr[i-1][4]) / arr[i-1][4]; //小數點 還不是百分比
-        }
-    }
-
-    void intraday_return(){ // 要先sort by date才可以用，計算intraday return並放在column 6
-        for(int i=0;i<size;i++){
-            arr[i][6] = (arr[i][4] - arr[i][1]) / arr[i][1]; //小數點 還不是百分比
-        }
-    } 
 
     void show(){ //測試用
         for(int i=0;i<10;i++){
             for(int j=0;j<5;j++){
-                if(j==0) cout << int(arr[i][j]) << " ";
-                else cout << arr[i][j] << " ";
-                
+                if(j==0) cout << int(arr[i].close) << " ";
+                else cout << arr[i].close << " ";
             }
             cout << endl;
         }
--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 #include <string>
 #include <cstring>
 #include <fstream>
+#include <algorithm>
 #include "MaxHeap.cpp"
 using namespace std;
+
+//col 0    1    2    3   4     5            6
+//    date open high low close daily_return intraday_return
+
+
+
 
 int main(){
     //讀取檔案
@@ -50,7 +57,7 @@ int main(){
     } 
     size = i;//unique date數量(沒有重複日期)
     MaxHeap heap(size);//建立MaxHeap array
-    double temp[7];
+    stock temp;
     i = 0;
     int cut;
     while (getline(in, line)){
@@ -65,96 +72,109 @@ int main(){
         if(!is_unique){
             continue;
         }
-        temp[0] = stod(line.substr(0, cut));
+        temp.date = line.substr(0, cut);
         line = line.substr(cut + 1);
         
         cut = line.find(",");
-        temp[1] = stod(line.substr(0, cut));
+        temp.open = stod(line.substr(0, cut));
         line = line.substr(cut + 1);
 
         cut = line.find(",");
-        temp[2] = stod(line.substr(0, cut));
+        temp.high = stod(line.substr(0, cut));
         line = line.substr(cut + 1);
 
         cut = line.find(",");
-        temp[3] = stod(line.substr(0, cut));
+        temp.low = stod(line.substr(0, cut));
         line = line.substr(cut + 1);
 
-        temp[4] = stod(line);
+        temp.close = stod(line);
         
         heap.insert(i, temp);
         i++;
     }
+    MaxHeap heapcopy = heap; //copy by date
+
     cout << "Task(A):" << endl;
     //(1) Determine how many unique dates are in the dataset.
     cout << "(1) There are "  << size << " unique dates in the dataset." << endl;
 
     //(2) Find the 10 smallest prices and which dates contain these smallest prices.
-    heap.heapSort(4); //sort by close price
+    heapcopy.heapSort(); //sort by close price
     cout << "(2) The 10 smallest prices are:" << endl;
     for(int i=0;i<10;i++){
-        cout << heap.get(i, 4) << " on date " << int(heap.get(i, 0)) << endl;
+        cout << heapcopy.get(i).close << " on date " << heapcopy.get(i).date << endl;
     }
     
     //(3) Find the 10 largest prices and which dates contain these largest prices.
     cout << "(3) The 10 largest prices are:" << endl;
     for(int i=size-10;i<size;i++){
-        cout << heap.get(i, 4) << " on date " << int(heap.get(i, 0)) << endl;
+        cout << heapcopy.get(i).close << " on date " << heapcopy.get(i).date << endl;
     }
 
     //(4) Find the median price and its occurring date
-    cout << "(4) The median price is " << heap.get(size/2, 4) << " and its occurring date is " << int(heap.get(size/2, 0)) << endl;
+    cout << "(4) The first median price is " << heapcopy.get(size/2-1).close << " and its occurring date is " << heapcopy.get(size/2-1).date << endl;
+    cout << "    The second median price is " << heapcopy.get(size/2).close << " and its occurring date is " << heapcopy.get(size/2).date << endl;
 
     //(5) Compute the daily return for every day (except the first day). Then determine what the 
     // maximum and minimum returns (return could be a negative value) are and on which day(s) they occur.
-    heap.heapSort(0); //sort by date
-    heap.daily_returns();
-    double max_return = 0;
-    double min_return = INT_MAX;
-    int max_index;
-    int min_index;
-    for(int i=1;i<size;i++){
-        if(heap.get(i, 5) > max_return){
-            max_return = heap.get(i, 5);
-            max_index = i;
+    
+    double daily_returns[size];
+    double max_return=0, min_return=INT_MAX;
+    string max_date, min_date;
+    for(int i=0;i<size-1;i++){
+        daily_returns[i]=(heap.get(i+1).close-heap.get(i).close) / heap.get(i).close * 100;
+        if(daily_returns[i] > max_return){
+            max_return = daily_returns[i];
+            max_date = heap.get(i+1).date;
         }
-        if(heap.get(i, 5) < min_return){
-            min_return = heap.get(i, 5);
-            min_index = i;
+        if(daily_returns[i] < min_return){
+            min_return = daily_returns[i];
+            min_date = heap.get(i+1).date;
         }
     }
-    cout << "(5) The maximum return is " << max_return*100 << "%" << " and it occurs on " << int(heap.get(max_index, 0)) << endl;
-    cout << "    The minimum return is " << min_return*100 << "%" << " and it occurs on " << int(heap.get(min_index, 0)) << endl;
+    cout << "(5) The maximum return is " << max_return << "%" << " and it occurs on " << max_date << endl;
+    cout << "    The minimum return is " << min_return << "%" << " and it occurs on " << min_date << endl;
     
     //(6) Compute the intraday return for every day. Then determine what the maximum and
     // minimum returns (return could be a negative value) are and on which day(s) they occur.
-    heap.intraday_return();
-    max_return = 0;
-    min_return = INT_MAX;
-     for(int i=0;i<size;i++){
-        if(heap.get(i, 6) > max_return){
-            max_return = heap.get(i, 6);
-            max_index = i;
+    double intraday_return[size];
+    max_return=0, min_return=INT_MAX;
+    for(int i=0;i<size;i++){
+        daily_returns[i]=(heap.get(i).close-heap.get(i).open) / heap.get(i).open * 100;
+        if(daily_returns[i] > max_return){
+            max_return = daily_returns[i];
+            max_date = heap.get(i).date;
         }
-        if(heap.get(i, 6) < min_return){
-            min_return = heap.get(i, 6);
-            min_index = i;
+        if(daily_returns[i] < min_return){
+            min_return = daily_returns[i];
+            min_date = heap.get(i).date;
         }
     }
-    cout << "(6) The maximum return is " << max_return*100 << "%" << " and it occurs on " << int(heap.get(max_index, 0)) << endl;
-    cout << "    The minimum return is " << min_return*100 << "%" << " and it occurs on " << int(heap.get(min_index, 0)) << endl;
+    cout << "(6) The maximum return is " << max_return << "%" << " and it occurs on " << max_date << endl;
+    cout << "    The minimum return is " << min_return << "%" << " and it occurs on " << min_date << endl;
 
     //(10) Find the maximum, minimum and median prices using all the 4 columns of prices 
     // (i.e., Open_price, High_price, Low_price and Close_price) and determine on which date they occur.
-    cout << "(10)" << endl;
-    string priceTypes[4] = {"Open_price", "High_price", "Low_price", "Close_price"};
-    for (int i = 1; i <= 4; i++) {
-        heap.heapSort(i);
-        cout << priceTypes[i-1] << ":" << endl;
-        cout << "The maximum is " << heap.get(size-1, i) << " and it occurs on date " << int(heap.get(size-1, 0)) << endl;
-        cout << "The minimum is " << heap.get(0, i) << " and it occurs on date " << int(heap.get(0, 0)) << endl;
-        cout << "The median is " << heap.get(size/2, i) << " and it occurs on date " << int(heap.get(size/2, 0)) << endl;
+    int n=size*4;
+    MaxHeap all_price(n);
+    stock price;
+    for(int i=0;i<size;i++){ //第i天的四個價格
+        price.date = heap.get(i).date;
+        //四種價格都放入heap的close price
+        price.close = heap.get(i).open;
+        all_price.insert(i*4, price);
+        price.close = heap.get(i).high;
+        all_price.insert(i*4+1, price);
+        price.close = heap.get(i).low;
+        all_price.insert(i*4+2, price);
+        price.close = heap.get(i).close;
+        all_price.insert(i*4+3, price);
     }
+    all_price.heapSort();
+    cout << "(10) The maximum prices are: " << all_price.get(n-1).close << " on date " << all_price.get(n-1).date << endl;
+    cout << "     The minimum prices are: " << all_price.get(0).close << " on date " << all_price.get(0).date << endl;
+    cout << "     The first median prices are: " << all_price.get(n/2-1).close << " on date " << all_price.get(n/2-1).date << endl;
+    cout << "     The second median prices are: " << all_price.get(n/2).close << " on date " << all_price.get(n/2).date << endl;
     in.close();
     return 0;
 }
